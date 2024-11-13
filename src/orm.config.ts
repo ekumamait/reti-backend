90576ddcde5e7667a13f6972b96ffc1929161857
conquest-bishop <102058143+conquest-bishop@users.noreply.github.com>
2024-11-13 12:01:16 +0300

    Update orm.config.ts

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -10,6 +10,6 @@ export const config: TypeOrmModuleOptions = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    entities: ["dist/**/*.entity{.ts,.js}"],
     synchronize: false
 }
