e86699e2c1fbe9bd4c960026b9f8e0f7c9499606
Tony Ojelel <tonyojelel@gmail.com>
2024-11-14 14:07:31 +0300

    fix: remove seeding files from eslint PR

--- src/seeds/UserSeed.ts
+++
@@ -1,8 +0,0 @@
-import { Factory, times } from 'typeorm-seeding';
-import { User } from '../entities/User'; // Adjust the path to your User entity
-
-export default class UserSeed {
-  public async run(factory: Factory): Promise<void> {
-    await factory(User)().createMany(10); // Create 10 users
-  }
-}
