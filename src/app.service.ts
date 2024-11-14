dddabcabbde2b22f855accda2e288742e3dfb786
Tony Ojelel <tonyojelel@gmail.com>
2024-11-14 14:01:29 +0300

    fix: revert app.service.ts to original state

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,21 +1,8 @@
-// import { Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common';
 
-// @Injectable()
-// export class AppService {
-//   getHello(): string {
-//     return 'Hello World. This is!!' ;
-//   }
-// }
-
+@Injectable()
 export class AppService {
   getHello(): string {
-    const message = 'Hello World from test!';
-    return message;
-  }
-
-  // Adding a new method with poor formatting
-  testLinting(): void {
-    const x: any = 'test';
-    console.log(x);
+    return 'Hello World!';
   }
 }
