13930d8f70540739f642f877211494e6a0b1e5c5
kenyonyozi <kenyonyozibwengye@gmail.com>
2024-11-20 20:06:24 +0300

    fix seeding and introduce synchronized entity creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { config } from './db/data-source';
-import { UsersModule } from './users/users.module';
+import { config } from '../ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false,
+      }),
+    }),
+  ],
 })
 export class AppModule {}
