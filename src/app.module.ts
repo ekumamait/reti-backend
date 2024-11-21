894f8788986ce6851fd93bbae6a47ea758b88668
ekumamait <ericebuluochol@google.com>
2024-11-21 12:38:31 +0300

    undo changes not required

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { ProfileModule } from './profile/profile.module';
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
+        synchronize: false,
       }),
     }),
     UsersModule,
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { config } from 'dotenv';
 
 config();
 
-async function bootstrap(): Promise<void> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
