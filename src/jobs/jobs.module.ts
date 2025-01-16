c66b6f4f95250fb3d7b5b056f7ad313450572e74
Nta1e <ericebuluochol@gmail.com>
2025-01-16 13:55:41 +0300

    update returned notification message

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -4,9 +4,10 @@ import { JobsController } from './jobs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Job } from 'src/database/entities/job.entity';
 import { User } from 'src/database/entities/user.entity';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Job, User])],
+  imports: [TypeOrmModule.forFeature([Job, User]), NotificationsModule],
   controllers: [JobsController],
   providers: [JobsService],
 })
