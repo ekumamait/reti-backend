efbb98db0a4f020c2e99f1b30b7b0f57599ee047
Nta1e <ericebuluochol@gmail.com>
2025-01-27 12:19:32 +0300

    fix deployment scripts

--- ormconfig.seed.js
+++ ormconfig.seed.js
@@ -5,7 +5,7 @@ module.exports = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [__dirname + '/dist/**/*.entity.js'],
   migrations: [__dirname + '/dist/database/migrations/*.js'],
