c0d009536e75aed880c988802561d797318233b6
conquest-bishop <102058143+conquest-bishop@users.noreply.github.com>
2025-03-04 15:11:17 +0300

    Revert "Add opportunity email when user applies for a job"

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,15 +11,9 @@ import { InspirationsModule } from './inspirations/inspirations.module';
 import { MentorshipSessionsModule } from './mentorship-sessions/mentorship-sessions.module';
 import { ChatGateway } from './chat/chat.gateway';
 import { SupportModule } from './support/support.module';
-import { JobEmailModule } from './opportunity/opportunity.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { join } from 'path';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
@@ -32,26 +26,6 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
         synchronize: true,
       }),
     }),
-    MailerModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        transport: {
-          service: 'Gmail',
-          auth: {
-            user: config.getOrThrow('SMTP_USER'),
-            pass: config.getOrThrow('SMTP_PASSWORD'),
-          },
-        },
-        template: {
-          dir: join(__dirname, 'templates'),
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
-          },
-        },
-      }),
-      inject: [ConfigService],
-    }),
     UsersModule,
     AuthModule,
     ProfileModule,
@@ -61,7 +35,6 @@ import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handleba
     JobsModule,
     InspirationsModule,
     MentorshipSessionsModule,
-    JobEmailModule,
     SupportModule,
   ],
   providers: [ChatGateway],
